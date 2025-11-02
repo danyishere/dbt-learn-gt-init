137fb4320e8cef5bb03bf125089b7afb7bd5ad39
Dany Boy <dpe.moutou@gmail.com>
2025-11-03 00:19:17 +0100

    DMO-1103-(Add metrics + Remove cent to dollar macro for Dbt Cloud)

--- macros/cent_to_dollar.sql
+++
@@ -1,3 +0,0 @@
-{% macro cent_to_dollar(col_name) -%}
-    {{ col_name }} / 100
-{%- endmacro %}
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -4,7 +4,7 @@ select
     id as payment_id,
     paymentmethod as payment_method,
     status,
-    {{ cent_to_dollar('amount') }} as amount,
+    amount/100 as amount,
     created as created_at
 
 from {{ source('stripe', 'payment') }}
